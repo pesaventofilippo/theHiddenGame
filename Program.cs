c3d2eb002666d724085c4896fb4c0de29f53961c
Filippo Pesavento <pesaventofilippo@gmail.com>
2019-11-13 17:29:50 +0100

    Added first debug code

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace theHiddenGame {
+    class MainClass {
+        public static void Main(string[] args) {
+            Console.WriteLine("-- theHiddenGame --");
+            ConsoleKeyInfo keyInfo;
+            while ((keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Escape) {
+                switch (keyInfo.Key) {
+                    case ConsoleKey.UpArrow:
+                        Console.WriteLine("Up Arrow!");
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        Console.WriteLine("Down Arrow!");
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        Console.WriteLine("Left Arrow!");
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        Console.WriteLine("Right Arrow!");
+                        break;
+                }
+            }
+        }
+    }
+}
